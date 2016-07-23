8eaa09077abc1eee76110622b68c1daebdd0c734
Ghanan Gowripalan <ghanan94@gmail.com>
2016-07-23 14:31:42 -0400

    Initial commit for initial prototype matlab code

---
+++ initial_prototype/initial_prototype_sim.m
@@ -0,0 +1,18 @@
+%
+% NAME:        initial_prototype_sim
+%
+% DESCRIPTION: Runs a simulation for the intial prototype of FYDP Project,
+%              CommBlocker (reduce ambient noise at source).
+%
+% PARAMETERS:
+%   N/A
+%
+% RETURNS:
+%   N/A
+%
+function [] = initial_prototype_sim()
+
+
+
+end
+
